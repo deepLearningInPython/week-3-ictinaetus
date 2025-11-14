7ba5b231d4d8fb53af55f8491da1748fcdcadfb7
Alan <ictinaetus@gmail.com>
2025-11-14 18:42:09 +0100

    Fix: Add numpy import to task 1 test2

--- grade_task1.py
+++ grade_task1.py
@@ -1,3 +1,7 @@
+import numpy as np
+from tasks import *
+from numpy.testing import assert_array_equal
+
 def test_compute_output_size_1d():
     input_array = np.array([1, 2, 3, 4, 5])
     kernel_array = np.array([1, 0, -1])
